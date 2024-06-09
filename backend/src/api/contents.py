29c769d867c5e0c33b98ba8c7201a5563c48439c
Tiago Henrique <tiagohrpg@gmail.com>
2024-06-08 21:23:51 -0300

    feat(contents): add post and get movies routes

--- backend/src/api/contents.py
+++ backend/src/api/contents.py
@@ -1,7 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from src.schemas.movie_content import Movie
+from src.db.database import getDB, saveDB
 
 router = APIRouter()
 
-@router.get("/")
-def get_contents():
-    return ["my contents"]
+
+@router.post("/add_movie", status_code=201, tags=["movie"], response_model=Movie)
+async def add_movie(movie: Movie):
+    db = getDB()
+
+    movie_dict = movie.model_dump()
+    print(movie)
+    for _movie in db["movies"]:
+        if movie_dict["title"] == _movie["title"]:
+            raise HTTPException(status_code=422, detail="There is a movie with the same title in the database")
+
+    db["movies"].append(movie_dict)
+    saveDB(db)
+    return movie
+
+
+@router.get("/", status_code=200, tags=["movie"], response_model=list[Movie])
+async def get_contents():
+    db = getDB()
+
+    return db["movies"]

660d64207c92eecf9943cfdcbfc26dbad6cebce7
Tiago Henrique <tiagohrpg@gmail.com>
2024-06-11 20:36:18 -0300

    test(movie): add movie api tests and scenarios

---
+++ backend/src/tests/api/features/movies.feature
@@ -0,0 +1,59 @@
+Feature: Movies API
+
+    Scenario: Register a movie on the database
+        Given no movie with title "Gato de botas" is in the database
+        When a POST request is sent to "/contents/movies" with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+        Then the json status code is "201"
+        And the json response have title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+
+    Scenario: Try to register a movie on the database with title that already exists
+        Given only movie with title "Gato de botas" is in the database
+        When a POST request is sent to "/contents/movies" with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "terror", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+        Then the json status code is "422"
+        And the json response have message "There is a movie with the same title in the database"
+        And only "1" movie is in the database
+
+    Scenario: get a movie by title on the database
+        Given only movie with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png" is in the database
+        When a GET request is sent to "/contents/movies/Gato%20de%20botas"
+        Then the json status code is "200"
+        And the json response have title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+
+    Scenario: get a movie by title that does not exist on the database 
+        Given no movie with title "Gato de botas" is in the database
+        When a GET request is sent to "/contents/movies/Gato%20de%20botas"
+        Then the json status code is "404"
+        And the json response have message "No movie with this title found in the database"
+
+    Scenario: Update a movie on the database
+        Given only movie with title "Gato de botas" is in the database
+        When a PUT request is sent to "/contents/movies/Gato%20de%20botas" with title "Gato de botas 2", synopsis "Uma nova história de um gato com habilidades de espadachim e uma bota. O gato enfrenta muitos desafios em sua jornada cativante", gender "Ação", duration "125", release year "2024", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+        Then the json status code is "200"
+        And the json response have title "Gato de botas 2", synopsis "Uma nova história de um gato com habilidades de espadachim e uma bota. O gato enfrenta muitos desafios em sua jornada cativante", gender "aventura", duration "125", release year "2024", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+        And only "1" movie is in the database
+        And movie with title "Gato de botas 2" is in the database
+
+    Scenario: Try to update a movie on the database with title that does not exist
+        Given only movie with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png" is in the database
+        When a POST request is sent to "/contents/movies/Matrix" with title "Matrix", synopsis "Uma distopia cheia de mistérios e grandes revelações", gender "ação", duration "150", release year "2008", director "Tarantino", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "matrix.png"
+        Then the json status code is "404"
+        And the json response have message "No movie with this title found in the database"
+        And only "1" movie is in the database
+        And movie with title "Gato de botas" is in the database
+
+    Scenario: delete a movie by title on the database
+        Given only movie with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png" is in the database
+        And movie with title "Matrix", synopsis "Uma distopia cheia de mistérios e grandes revelações", gender "ação", duration "150", release year "2008", director "Tarantino", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "matrix.png" is in the database
+        When a DELETE request is sent to "/contents/movies/Gato%20de%20botas"
+        Then the json status code is "200"
+        And the json response have title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png"
+        And only "1" movie is in the database
+        And movie with title "Matrix" is in the database
+
+    Scenario: delete a movie by title that does not exist on the database 
+        Given only movie with title "Gato de botas", synopsis "A história de um gato com habilidades de espadachim e uma bota", gender "aventura", duration "125", release year "2022", director "The Rock", main cast "Rodrigo Hilbert, Madonna, Xuxa", banner "gato_de_botas.png" is in the database
+        When a DELETE request is sent to "/contents/movies/Matrix"
+        Then the json status code is "404"
+        And the json response have message "No movie with this title found in the database"
+        And only "1" movie is in the database
+        And movie with title "Gato de botas" is in the database
---
+++ backend/src/tests/api/step_definitions/test_movies.py
@@ -0,0 +1,207 @@
+from src.db.database import getDB, saveDB, clearDB
+from pytest_bdd import parsers, given, when, then, scenario
+from src.service.impl.content_service import ContentService
+from src.schemas.content import Movie
+
+
+@scenario(scenario_name="Register a movie on the database", feature_name="../features/movies.feature")
+def test_register_movie():
+    pass
+
+@given(parsers.cfparse('no movie with title "{movie_title}" is in the database'))
+def mock_movie_service_clean(movie_title: str):
+    db = getDB()
+    clearDB(db)
+
+@when(
+    parsers.cfparse('a POST request is sent to "{req_url}" with title "{title}", synopsis "{synopsis}", gender "{gender}", duration "{duration}", release year "{release_year}", director "{director}", main cast "{main_cast}", banner "{banner}"'), 
+    target_fixture="context"
+)
+def send_post_movie_request(client, context, req_url: str, title: str, synopsis: str, gender: str, duration: str, release_year:str, director: str, main_cast: str, banner: str):
+    main_cast_list = main_cast.split(",")
+
+    response = client.post(
+        req_url,
+        json={
+            "title": title,
+            "synopsis": synopsis,
+            "gender": gender,
+            "duration": int(duration),
+            "release_year": int(release_year),
+            "director": director,
+            "main_cast": main_cast_list,
+            "banner": banner
+        })
+    
+    context["response"] = response
+    return context
+
+@then(parsers.cfparse('the json status code is "{status_code}"'), target_fixture="context")
+def check_json_status_code(context, status_code: str):
+    assert context["response"].status_code == int(status_code)
+
+    return context
+
+@then(parsers.cfparse('the json response have title "{title}", synopsis "{synopsis}", gender "{gender}", duration "{duration}", release year "{release_year}", director "{director}", main cast "{main_cast}", banner "{banner}"'), target_fixture="context")
+def check_response_json(context, title: str, synopsis: str, gender: str, duration: str, release_year:str, director: str, main_cast: str, banner: str):
+    
+    json_response = context["response"].json()
+    assert json_response["title"] == title
+    assert json_response["synopsis"] == synopsis
+    assert json_response["duration"] == int(duration)
+    assert json_response["release_year"] == int(release_year)
+    assert json_response["director"] == director
+    assert json_response["main_cast"] == main_cast.split(",")
+    assert json_response["banner"] == banner
+
+    return context
+
+
+"""registrar filme com mesmo título"""
+@scenario(scenario_name="Try to register a movie on the database with title that already exists", feature_name="../features/movies.feature")
+def test_register_movie_same_title():
+    pass
+
+@given(parsers.cfparse('only movie with title "{movie_title}" is in the database'))
+def mock_movie_service_creation(movie_title: str):
+    db = getDB()
+    clearDB(db)
+
+    ContentService.add_content(Movie(
+        title=movie_title, 
+        synopsis="random synopsis",
+        gender="random gender",
+        duration=0,
+        release_year=0,
+        director="random director",
+        main_cast=[]
+    ), "movies")
+
+@then(parsers.cfparse('the json response have message "{message}"'), target_fixture="context")
+def check_json_status_code(context, message: str):
+    assert context["response"].json()["detail"] == message
+
+    return context
+
+@then(parsers.cfparse('only "{num_movies}" movie is in the database'), target_fixture="context")
+def check_json_status_code(context, num_movies: str):
+    db = getDB()
+    assert len(db["movies"]) == int(num_movies)
+
+    return context
+
+
+"""GET movie by title"""
+@scenario(scenario_name="get a movie by title on the database", feature_name="../features/movies.feature")
+def test_get_movie_by_title():
+    pass
+
+@given(parsers.cfparse('only movie with title "{title}", synopsis "{synopsis}", gender "{gender}", duration "{duration}", release year "{release_year}", director "{director}", main cast "{main_cast}", banner "{banner}" is in the database'))
+def mock_movie_service_creation(title: str, synopsis: str, gender: str, duration: str, release_year:str, director: str, main_cast: str, banner: str):
+    db = getDB()
+    clearDB(db)
+
+    ContentService.add_content(Movie(
+        title=title, 
+        synopsis=synopsis,
+        gender=gender,
+        duration=int(duration),
+        release_year=int(release_year),
+        director=director,
+        main_cast=main_cast.split(","),
+        banner=banner
+    ), "movies")
+
+@when(
+    parsers.cfparse('a GET request is sent to "{req_url}"'), 
+    target_fixture="context"
+)
+def send_get_movie_request(client, context, req_url: str):
+
+    response = client.get(req_url)
+    
+    context["response"] = response
+    return context
+
+"""GET movie by title that does not exist"""
+@scenario(scenario_name="get a movie by title that does not exist on the database", feature_name="../features/movies.feature")
+def test_get_movie_by_title_not_exist():
+    pass
+
+"""update movie on the database"""
+@scenario(scenario_name="Update a movie on the database", feature_name="../features/movies.feature")
+def test_update_movie():
+    pass
+
+@when(
+    parsers.cfparse('a PUT request is sent to "{req_url}" with title "{title}", synopsis "{synopsis}", gender "{gender}", duration "{duration}", release year "{release_year}", director "{director}", main cast "{main_cast}", banner "{banner}"'), 
+    target_fixture="context"
+)
+def send_post_movie_request(client, context, req_url: str, title: str, synopsis: str, gender: str, duration: str, release_year:str, director: str, main_cast: str, banner: str):
+    main_cast_list = main_cast.split(",")
+
+    response = client.put(
+        req_url,
+        json={
+            "title": title,
+            "synopsis": synopsis,
+            "gender": gender,
+            "duration": int(duration),
+            "release_year": int(release_year),
+            "director": director,
+            "main_cast": main_cast_list,
+            "banner": banner
+        })
+    
+    context["response"] = response
+    return context
+
+@then(parsers.cfparse('movie with title "{title}" is in the database'), target_fixture="context")
+def check_json_status_code(context, title: str):
+    db = getDB()
+    found = False
+    for movie in db["movies"]:
+        if movie["title"] == title:
+            found = True
+
+    assert found == True
+
+    return context
+
+"""update movie on the database"""
+@scenario(scenario_name="Update a movie on the database", feature_name="../features/movies.feature")
+def test_update_movie_not_exist():
+    pass
+
+"""delete movie by title"""
+@scenario(scenario_name="delete a movie by title on the database", feature_name="../features/movies.feature")
+def test_delete_movie():
+    pass
+
+@given(parsers.cfparse('movie with title "{title}", synopsis "{synopsis}", gender "{gender}", duration "{duration}", release year "{release_year}", director "{director}", main cast "{main_cast}", banner "{banner}" is in the database'))
+def mock_movie_service_creation(title: str, synopsis: str, gender: str, duration: str, release_year:str, director: str, main_cast: str, banner: str):
+    ContentService.add_content(Movie(
+        title=title, 
+        synopsis=synopsis,
+        gender=gender,
+        duration=int(duration),
+        release_year=int(release_year),
+        director=director,
+        main_cast=main_cast.split(","),
+        banner=banner
+    ), "movies")
+
+@when(
+    parsers.cfparse('a DELETE request is sent to "{req_url}"'), 
+    target_fixture="context"
+)
+def send_get_movie_request(client, context, req_url: str):
+    response = client.delete(req_url)
+    
+    context["response"] = response
+    return context
+
+"""delete movie by title that does not exist"""
+@scenario(scenario_name="delete a movie by title that does not exist on the database", feature_name="../features/movies.feature")
+def test_delete_movie_not_exist():
+    pass

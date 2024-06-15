56a5eecd038ac5ce275c6f0a385fb1e006a9bf12
gustavosalm <gustavosalmeida9821@gmail.com>
2024-06-15 16:58:30 -0300

    feat(watch_list): added watch list routes to api

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from src.api import items
 from src.api import contents
 from src.api import reviews
+from src.api import watch_list
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(contents.router, prefix="/contents", tags=["contents"])
 api_router.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
+api_router.include_router(watch_list.router, prefix="/watch_list", tags=["watch_list"])

6d06b857186f1c1a56c32e687e9443b12db14713
gustavosalm <gustavosalmeida9821@gmail.com>
2024-06-15 16:59:13 -0300

    feat(watch_list): added watch list services

---
+++ backend/src/service/impl/watch_list_service.py
@@ -0,0 +1,35 @@
+from src.schemas.content import Movie, TvShow, Category
+from src.db.database import getDB, saveDB
+
+class WatchListService:
+    @staticmethod
+    def add_to_category_list(username: str, category_id: str, content_id: str, content_type: str):
+        db = getDB()
+
+        for content in db[content_type]:
+            if content["id"] == content_id:
+                db[username][category_id].append(content)
+                saveDB(db)
+                return content
+
+        return None
+
+    @staticmethod
+    def get_category_list(username: str, category_id: str):
+        db = getDB()
+
+        return db[username][category_id]
+    
+    @staticmethod
+    def delete_of_category_list(username: str, category_id: str, content_id: str, content_type: str):
+        db = getDB()
+
+        for i in range(len(db[username][category_id])):
+            if db[username][category_id][i]["id"] == content_id:
+                deleted_content = db[username][category_id].pop(i)
+                saveDB(db)
+                return deleted_content
+                break
+
+        return None
+

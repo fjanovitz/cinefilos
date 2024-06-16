8c8ff11c7c39278555d372c12125d501ea3d24f1
Frederico Janovitz <fjn@cin.ufpe.br>
2024-06-16 00:30:22 -0300

    fix(posts): removed services that will not be used
    fix(posts): removed services that will not be used(again)

--- backend/src/service/impl/post_service.py
+++ backend/src/service/impl/post_service.py
@@ -2,10 +2,6 @@ from src.schemas.post import Post
 from src.db.database import getDB, saveDB
 
 class PostService:
-    @staticmethod
-    def get_posts():
-        db = getDB()
-        return db["posts"]
 
     @staticmethod
     def get_posts_by_topic(topic: str):
@@ -32,82 +28,3 @@ class PostService:
             if post["author"] == author:
                 posts.append(post)
         return posts
-
-    @staticmethod
-    def add_post(post: Post):
-        db = getDB()
-
-        post_dict = post.model_dump()
-        for post_ in db["posts"]:
-            if post_dict == post_:
-                return None
-            
-        db["posts"].append(post_dict)
-        saveDB(db)
-        return post
-    
-    @staticmethod
-    def delete_post(post_id: str):
-        db = getDB()
-
-        found = False
-        for i in range(len(db["posts"])):
-            if db["posts"][i]["id"] == post_id:
-                found = True
-                deleted_post = db["posts"].pop(i)
-                break
-
-        if not found:
-            return None
-
-        saveDB(db)
-        return deleted_post
-
-    @staticmethod
-    def add_like(post_id: str, user: User):
-        db = getDB()
-
-        for post in db["posts"]:
-            if post["id"] == post_id:
-                post["users_who_liked"].append(user)
-                post["num_likes"] += 1
-                saveDB(db)
-                return post
-
-        return None
-    
-    @staticmethod
-    def remove_like(post_id: str, user_id: str):
-        db = getDB()
-
-        found_post = False
-        for post_ in db["posts"]:
-            if post_["id"] == post_id:
-                found_post = True
-                post = post_
-        
-        if not found_post:
-            return None
-
-        found_user = False
-        for i in range(len(post["users_who_liked"])):
-            if post["users_who_liked"][i]["id"] == user_id:
-                found_user = True
-                removed_user = post["users_who_liked"].pop(i)
-                break
-
-        if not found_user:
-            return None
-
-        saveDB(db)
-        return removed_user
-    
-    @staticmethod
-    def get_likes_by_post(post_id: str):
-        db = getDB()
-        
-        for post in db["posts"]:
-            if post["id"] == post_id:
-                return post["users_who_liked"]
-                
-        return None

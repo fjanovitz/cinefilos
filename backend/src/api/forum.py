379afb5d9bf467c29d1b63ac1a5a47d48e343232
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-06-16 16:58:12 -0300

    add forum feature

---
+++ backend/src/api/forum.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from src.db.database import getDB
+
+router = APIRouter()
+
+@router.get("/forum", status_code = 200, tags = ["forum"], response_model=list[Post])
+async def get_posts():
+    db = getDB()
+
+    return db["posts"]
+
+@router.get("/forum/{post_id}", status_code = 200, tags = ["forum"], response_model=Post)
+async def get_post(post_id: str):
+    db = getDB()
+
+    for post in db["posts"]:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code = 404, detail = "No post with this id found in the database")
+
+@router.put("/forum/{post_id}", status_code = 200, tags = ["forum"], response_model=Post)
+async def update_post(post_id: str, post: Post):
+    db = getDB()
+
+    for i in range(len(db["posts"])):
+        if db["posts"][i]["id"] == post_id:
+            db["posts"][i] = post.dict()
+            return post
+    raise HTTPException(status_code = 404, detail = "No post with this id found in the database")

d11be99dcd7b58191a7e43ec861b368f38a07798
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-06-18 02:52:06 -0300

    fix comments tests

--- backend/src/tests/api/step_definitions/test_comments.py
+++ backend/src/tests/api/step_definitions/test_comments.py
@@ -5,23 +5,11 @@ from src.schemas.forum import Post, Comment
 
 # Feature: Comment on Posts
 
-#     Scenario: Add a comment to a post
-#         Given post with post_id "123" in the database
-#         When a POST request is sent to "/post/123" by user "ersaujo", content "This is a comment"
-#         Then the json status code is "200"
-#         And the json response contains the comment_id, author "ersaraujo", content "This is a comment"
-
-#     Scenario: Remove a comment from a post
-#         Given post with comment_id "789", author "ersaraujo", content "This is a comment"
-#         When a DELETE request is sent to "/post/789" by user "ersaraujo"
-#         Then the json status code is "200"
-#         And the json response contains the removed comment
-
-#     Scenario: Add a comment to a post with empty content
-#         Given post with post_id "123" in the database
-#         When a POST request is sent to "/post/123" by user "ersaujo", content ""
-#         Then the json status code is "400"
-#         And the json response contains the error message "Content cannot be empty"
+# Scenario: Add a comment to a post
+#     Given post with post_id "123" in the database
+#     When a POST request is sent to "/post/123" by user "ersaraujo", content "This is a comment"
+#     Then the json status code is "200"
+#     And the json response contains the comment_id, author "ersaraujo", content "This is a comment"
 
 @scenario(scenario_name="Add a comment to a post", feature_name="../features/comments.feature")
 def test_add_comment_to_post():
@@ -34,6 +22,7 @@ def mock_post_service_clean(post_id: str):
     clearDB(db)
 
     post = Post(
+        id=post_id,
         author="João",
         title="Lições - Puppy Love",
         content="Conteúdo",
@@ -49,17 +38,25 @@ def mock_post_service_add_comment(post_id: str, user: str, content: str):
         content=content
     )
 
+    if content == " ": content = None
     PostService().add_comment(post_id, post)
 
-@then(parsers.cfparse('the json status code is "{status_code}"'))
-def mock_post_service_check_status_code(status_code: str):
-    assert status_code == "200"
+@then(parsers.cfparse('the json response contains the comment_id, author "{author}", content "{content}"'), target_fixture="context")
+def mock_post_service_check_comment(context, author: str, content: str):
+    post = PostService().get_post_by_id("123")
+    comment = post["comments"][0]
+    assert comment["author"] == author
+    assert comment["content"] == content
+    context["comment_id"] = comment["id"]
+    return context
 
-@then(parsers.cfparse('the json response contains the comment_id, author "{author}", content "{content}"'))
-def mock_post_service_check_comment(author: str, content: str):
-    post = PostService().get_post("123")
-    assert post.comments[0].author == author
-    assert post.comments[0].content == content
+
+# Scenario: Remove a comment from a post
+#     Given post with post_id "123" in the database
+#     And post with comment_id "789", author "ersaraujo", content "This is a comment"
+#     When a DELETE request is sent to "/post/789" by user "ersaraujo"
+#     Then the json status code is "200"
+#     And the json response contains the post with post_id "123" and comment_id "789" removed
 
 @scenario(scenario_name="Remove a comment from a post", feature_name="../features/comments.feature")
 def test_remove_comment_from_post():
@@ -90,19 +87,22 @@ def mock_post_service_clean(comment_id: str, author: str, content: str):
 def mock_post_service_remove_comment(post_id: str, user: str):
     PostService().remove_comment(post_id, user)
 
-@then(parsers.cfparse('the json response contains the removed comment'))
-def mock_post_service_check_comment():
-    post = PostService().get_post("789")
-    assert len(post.comments) == 0
+@then(parsers.cfparse('the json response contains the post without comment_id "{comment_id}"'), target_fixture="context")
+def mock_post_service_check_comment(context, comment_id: str):
+    post = PostService().get_post_by_id("123")
+    for comment in post["comments"]:
+        assert comment["id"] != comment_id
+
+    return context
 
 @scenario(scenario_name="Add a comment to a post with empty content", feature_name="../features/comments.feature")
 def test_add_comment_to_post_empty_content():
     db = getDB()
     clearDB(db)
 
-@then(parsers.cfparse('the json status code is "{status_code}"'))
-def mock_post_service_check_status_code(status_code: str):
-    assert status_code == "400"
+@then(parsers.cfparse('the json status code is "{status_code}"'), target_fixture="context")
+def mock_post_service_check_status_code(context, status_code: str):
+    context["status_code"] = status_code
 
 @then(parsers.cfparse('the json response contains the error message "{error_message}"'))
 def mock_post_service_check_error_message(error_message: str):

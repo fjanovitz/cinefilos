e412281a468d21ec46f2d902b44c041e9fea4381
gustavosalm <gustavosalmeida9821@gmail.com>
2024-07-20 13:02:28 -0300

    feat(ContentCard): adding options button to card in progress

--- frontend/src/app/home/components/ContentListView/ContentCard.tsx
+++ frontend/src/app/home/components/ContentListView/ContentCard.tsx
@@ -22,27 +22,34 @@ const ContentCard: React.FC<ContentCardProps> = ({ content }) => {
 	const [isHovered, setIsHovered] = useState(false);
 
 	return (
-		<Link
-			to={`/contents/${content.content_type}/${content.title}`}
-			key={content.id}
-			className={styles.link}
-		>
-			<Card
-				className={styles.card}
-				onMouseEnter={() => setIsHovered(true)} 
-				onMouseLeave={() => setIsHovered(false)} 
+		<div className={styles.outerContainer}>
+			<Link
+				to={`/contents/${content.content_type}/${content.title}`}
+				key={content.id}
+				className={styles.link}
 			>
-				<Card.Img
-					variant="top"
-					src={content.banner}
-					className={styles.cardImg}
-				/>
+				<Card
+					className={styles.card}
+					onMouseEnter={() => setIsHovered(true)} 
+					onMouseLeave={() => setIsHovered(false)} 
+				>
+					<Card.Img
+						variant="top"
+						src={content.banner}
+						className={styles.cardImg}
+					/>
 
-				{isHovered && (
-					<div className={styles.titlePopup}>{content.title}</div>
-				)}
-			</Card>
-		</Link>
+					{isHovered && (
+						<div className={styles.titlePopup}>{content.title}</div>
+					)}
+				</Card>
+			</Link>
+			{isHovered && (
+				<div className={styles.optionsButton} onClick={(event) => {
+					console.log('testando isso aqui');
+				}}></div>
+			)}
+		</div>
 	);
 };
 

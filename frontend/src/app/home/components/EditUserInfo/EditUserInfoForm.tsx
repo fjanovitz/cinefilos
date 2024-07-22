e3df3fd70c2abb48df958bde71aedc3c87602c9a
clebs0n <clebsonsouza55@outlook.com>
2024-07-22 16:35:36 -0300

    fix : added styles to edit user info page

--- frontend/src/app/home/components/EditUserInfo/EditUserInfoForm.tsx
+++ frontend/src/app/home/components/EditUserInfo/EditUserInfoForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { updateUser, getUser } from '../../../../services/userService';
+import styles from '../../pages/EditUserPage/index.module.css';
 
 const EditUserInfoForm = () => {
   const { userId } = useParams();
@@ -29,7 +30,6 @@ const EditUserInfoForm = () => {
     };
     fetchUser();
   }, [userId]);
-  
 
   const handleChange = (e) => {
     setUserInfo({
@@ -45,74 +45,73 @@ const EditUserInfoForm = () => {
       return;
     }
     try {
-      // Fetch the current user data
       const currentUser = await getUser(userId);
-      
-      // Update the user fields within the data object
       currentUser.data.full_name = userInfo.full_name;
       currentUser.data.username = userInfo.username;
       currentUser.data.phone_number = userInfo.phone_number;
       currentUser.data.address = userInfo.address;
       currentUser.data.gender = userInfo.gender;
-      
-      // Send the entire user object in the request
       await updateUser(userId, currentUser.data);
     } catch (error) {
       console.error(error);
     }
   };
-  
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Full Name
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <div className={styles.inputGroup}>
+        <label className={styles.label}><b>Nome Completo:</b></label>
         <input
           type="text"
           name="full_name"
           value={userInfo.full_name}
           onChange={handleChange}
           required
+          className={styles.input}
         />
-      </label>
-      <label>
-        Username
+      </div>
+      <div className={styles.inputGroup}>
+        <label className={styles.label}><b>Nome de Usuário:</b></label>
         <input
           type="text"
           name="username"
           value={userInfo.username}
           onChange={handleChange}
           required
+          className={styles.input}
         />
-      </label>
-      <label>
-        Phone Number
+      </div>
+      <div className={styles.inputGroup}>
+        <label className={styles.label}><b>Número de Telefone:</b></label>
         <input
           type="text"
           name="phone_number"
           value={userInfo.phone_number}
           onChange={handleChange}
+          className={styles.input}
         />
-      </label>
-      <label>
-        Address
+      </div>
+      <div className={styles.inputGroup}>
+        <label className={styles.label}><b>Endereço:</b></label>
         <input
           type="text"
           name="address"
           value={userInfo.address}
           onChange={handleChange}
+          className={styles.input}
         />
-      </label>
-      <label>
-        Gender
+      </div>
+      <div className={styles.inputGroup}>
+        <label className={styles.label}><b>Gênero:</b></label>
         <input
           type="text"
           name="gender"
           value={userInfo.gender}
           onChange={handleChange}
+          className={styles.input}
         />
-      </label>
-      <button type="submit">Update Info</button>
+      </div>
+      <button type="submit" className={styles.button}>Atualizar Informações</button>
     </form>
   );
 };

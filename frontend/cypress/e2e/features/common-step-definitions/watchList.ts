3f92d098c9d32daba80ebf1adf61fd0894ad517b
gustavosalm <gustavosalmeida9821@gmail.com>
2024-07-22 20:11:11 -0300

    Test(Watch_List): added initial tests on watch list feature

--- frontend/cypress/e2e/features/common-step-definitions/watchList.ts
+++ frontend/cypress/e2e/features/common-step-definitions/watchList.ts
@@ -1,4 +1,15 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import api from '../../../../src/services/api';
+
+const postContent = async (username: string, category: string, content_id: string, content_type: string) => {
+    const params = {
+        username, 
+        category,
+        content_id,
+        content_type
+    }
+    const response = await api.post(`/watch_list/user/`, {}, {params});
+}
 
 Given(
     "the user is on the page {string}",
@@ -14,6 +25,20 @@ Given(
     }
 )
 
+Given(
+    "the movie with id {string}, type {string} is on the user {string} list of the user {string}",
+    (content_id: string, content_type: string, category: string, username: string) => {
+        postContent(username, category, content_id, content_type);
+    }
+)
+
+Given(
+    "the user {string} is not logged in",
+    (username: string) => {
+        console.log('n logado')
+    }
+)
+
 When(
     "the user clicks the {string} button for the movie with id {string}",
     (button: string, content_id: string) => {
@@ -26,6 +51,13 @@ When(
     (option_button: string) => {
         cy.getDataCy(option_button).click();
 });
+
+When(
+    "the user hovers the movie with id {string}",
+    (content_id: string) => {
+        cy.getDataCy(content_id).trigger('mouseover');
+    }
+)
 
 Then(
     "the movie with id {string} is not on the page",
@@ -52,5 +84,12 @@ Then(
     "the user is on the {string} category tab",
     (category: string) => {
         cy.getDataCy(category).children().should('be.disabled');
+    }
+)
+
+Then(
+    "the {string} button is not on the card",
+    (option_button: string) => {
+        cy.getDataCy(option_button).should('not.exist');
     }
 )
--- frontend/cypress/e2e/features/tests/watchList.feature
+++ frontend/cypress/e2e/features/tests/watchList.feature
@@ -11,16 +11,24 @@ Scenario: Change category tab
 
 Scenario: Change category of content
     Given the user is on the page "profile/edsonneto8"
+    And the movie with id "292177", type "movies" is on the user "assistidos" list of the user "edsonneto8"
     And the user is on the "watch_list_tab" tab
-    When the user clicks the "options_button" button for the movie with id "Magnum"
+    When the user clicks the "options_button" button for the movie with id "292177"
     And the user clicks the "quero_assistir_option" option
-    Then the movie with id "Magnum" is not on the page
+    Then the movie with id "292177" is not on the page
     When the user goes to the "quero_assistir_category" category tab
-    Then the movie with id "Magnum" is on the page
+    Then the movie with id "292177" is on the page
 
 Scenario: Remove content from category
     Given the user is on the page "profile/edsonneto8"
+    And the movie with id "292177", type "movies" is on the user "assistidos" list of the user "edsonneto8"
     And the user is on the "watch_list_tab" tab
-    When the user clicks the "options_button" button for the movie with id "Magnum"
+    When the user clicks the "options_button" button for the movie with id "292177"
     And the user clicks the "quero_assistir_option" option
-    Then the movie with id "Magnum" is not on the page
+    Then the movie with id "292177" is not on the page
+
+Scenario: Trying to add content to category not logged in
+    Given the user is on the page "contents/movies"
+    And the user "edsonneto8" is not logged in
+    When the user hovers the movie with id "292177"
+    Then the "options_button" button is not on the card

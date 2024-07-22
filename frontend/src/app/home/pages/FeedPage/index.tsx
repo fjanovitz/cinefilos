1cbeb61ac54fbdc4daf8cd95acbef2e0c4985538
fjanovitz <frednovais1208@gmail.com>
2024-07-22 15:19:21 -0300

    feat(posts): base for the likes page

--- frontend/src/app/home/pages/FeedPage/index.tsx
+++ frontend/src/app/home/pages/FeedPage/index.tsx
@@ -1,12 +1,28 @@
-import { useEffect, useState } from "react";
-import './index.module.css';
+import { Link } from "react-router-dom";
+import ContentListView from "../../components/ContentListView/ContentCardList";
+import styles from "./index.module.css";
 
-const FeedPage = () => {
-    return (
-        <div className="feed-page">
-            {/* Your feed content goes here */}
-        </div>
-    );
+const ContentsPage = ({ content_type }) => {
+	return (
+		<section>
+			<Link
+				to={{ pathname: `/contents/${content_type}/create_content` }}
+				state={{ content_type: content_type }}
+				style={{ textDecoration: "none" }}
+			>
+				<div className={styles.buttonCreateContentContainer}>
+					<button
+						type="submit"
+						className={styles.buttonCreateContent}
+            			data-cy="Adicionar Conteúdo"
+					>
+						Adicionar conteúdo
+					</button>
+				</div>
+			</Link>
+			<ContentListView content_type={content_type} />
+		</section>
+	);
 };
 
-export default FeedPage;
+export default ContentsPage;

94534d7394892f1876e520ec9cc4b77f214d9b96
clebs0n <95928023+clebs0n@users.noreply.github.com>
2024-07-23 18:32:18 -0300

    feat : added navigation menu in user pages

--- frontend/src/app/home/pages/EditUserPage/index.tsx
+++ frontend/src/app/home/pages/EditUserPage/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import EditUserInfoForm from '../../components/EditUserInfo/EditUserInfoForm';
 import styles from './index.module.css';
+import UserHeader from '../../components/Header/userHeader';
+
 
 const EditUserInfoPage = () => {
   return (
     <div className={styles.container}>
       <h1>Editar Informações do Usuário</h1>
+      <UserHeader />
       <EditUserInfoForm />
     </div>
   );

7de2ddfb2d16c25962d2780cfdc63c63072efa68
Tiago Henrique <tiagohrpg@gmail.com>
2024-07-22 22:30:16 -0300

    feat(*): add username to header when logged

--- frontend/src/app/home/components/Header/Header.tsx
+++ frontend/src/app/home/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css'
 import logo from '/src/shared/assets/logo.png'
+import { UserContext } from '../../context/UserContext';
 
 const Header: React.FC = () => {
+  const {user, saveUser} = useContext(UserContext);
   return (
     <header className={styles.headerContainer}>
       <div style={{ flex: 1 }}>
@@ -15,7 +17,7 @@ const Header: React.FC = () => {
         <Link to="/tests" style={{textDecoration: 'none', color: 'white'}} >Posts</Link>
       </nav>
       <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center', margin: '5px', gap: '10px' }}>
-        <Link to="/profile" style={{textDecoration: 'none', color: 'white'}} > Nome do Usuário </Link>
+        <Link to={`/profile/${user?.username ?? ""}`} style={{textDecoration: 'none', color: 'white'}} > {user?.username ?? "Nome do usuário"} </Link>
         <img src="https://cdn-icons-png.flaticon.com/512/147/147142.png" alt="Avatar" style={{ width: '40px', height: '40px', borderRadius: '50%' }} />
       </div>
     </header>
